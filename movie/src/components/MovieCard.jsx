79714623d28f14b694839d530c16fc30563eb800
sasha <sasha.park.designer@gmail.com>
2025-01-16 12:25:05 +0900

    MovieCard에 styled components 적용

--- movie/src/components/MovieCard.jsx
+++ movie/src/components/MovieCard.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import styled from 'styled-components'
 //map 메서드를 이용하여 영화 데이터를 MovieCard로  전달하세요.
+
+
+const Card = styled.div`
+display: flex;
+flex-direction: column;
+width: 200px;
+height: 300px;
+border: 1px solid black;
+border-radius: 10px;
+margin: 10px;
+padding: 10px;
+
+img {
+  width: 100%;
+  flex-grow: 1;
+  /* height: 70%; */
+}
+
+div {
+  padding: 10px;
+}
+
+
+`
+
+
+
 const MovieCard = () => {
+
+
+
+
   return (
     <>
-      <div>
-        <img src="" alt="" />
-        <div>제목</div>
-        <div>평점</div>
-      </div>
+
+        <Card>
+          <img src="" alt="a" />
+          <div>제목</div>
+          <div>평점</div>
+        </Card>
+
     </>
   );
 };
