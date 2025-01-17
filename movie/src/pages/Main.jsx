21687e8885f8100674d5d2756dfc534b679f0f44
sasha <sasha.park.designer@gmail.com>
2025-01-17 16:04:02 +0900

    env 파일 설정 🥹

--- movie/src/pages/Main.jsx
+++ movie/src/pages/Main.jsx
@@ -5,11 +5,16 @@ import movieDetailData from "../../public/data/movieDetailData.json";
 import styled from "styled-components";
 import Layout from "../Layout.jsx";
 import MovieSwiper from "../components/MovieSwiper.jsx";
-
-
+import { useEffect, useState } from "react";
+// import axios from "axios";
+// import require from "requirejs";
 
 export default function Main({ movies }) {
   // const pokemonData = useSelector(state => state.pokemon.data)
+  //vite에서 제공하는 환경변수에서 api키를 가져오는 방법.
+
+  const APIKEY = import.meta.env.VITE_TMDB_API_KEY;
+  // console.log("api키: ", APIKEY);
 
   const MovieList = styled.div`
     display: flex;
